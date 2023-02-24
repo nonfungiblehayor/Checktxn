14ca38aeff549d0df1d44dfc0ce7f1bcfe44c23c
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-02-24 02:45:52 -0800

    feat & style: created and styled chain text component

--- checktxn/src/App.jsx
+++ checktxn/src/App.jsx
@@ -1,13 +1,15 @@
 import style from './App.module.scss';
 import Header from './components/header';
 import SubHeader from './components/sub-header';
+import Chaintext from './components/chaintext';
 
 function App() {
   return (
     <div className={style.App}>
       <Header /> 
       <section className={style.mainBody}>
-        <SubHeader />        
+        <SubHeader />      
+        <Chaintext />  
         </section>   
     </div>
   );
---
+++ checktxn/src/components/chaintext.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from '../App.module.scss';
+
+function Chaintext() {
+    return <section className={style.chainTxt}>
+        <div className={style.exploreTxt}>
+            <h1>
+                Matic blockchain explorer
+            </h1>
+        </div>
+    </section>
+}
+
+export default Chaintext
