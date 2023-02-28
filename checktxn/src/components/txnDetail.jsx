b135a1cd94681c58f0912d8085f2eb892f931cf8
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-02-28 09:16:10 -0800

    style: styled txnStatus element

--- checktxn/src/components/txnDetail.jsx
+++ checktxn/src/components/txnDetail.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import style from '../App.module.scss';
 import mark from '../images/mark.webp';
 
-function Txndetail() {
-    const hash = '0x7600f6e76894dc7d928132d0aabe01e7d6e1171fbbc2f1798c25d582aa3858e0'
-    async function getTxn()  {
-        let response = await fetch(`https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash=${hash}&apikey=PBA7B9N9ZZR8UMSN7S5C16Q1YWXAZAUQBU`)
-        let data = await response.json();
-        let rcvData = data.result.status;
-        console.log(rcvData) 
-    }
+function Txndetail(props) {
     return <section className={style.txnSection}>
         <div className={style.txnContainer}>
         <div className={style.div1}>
@@ -30,7 +23,6 @@ function Txndetail() {
             </div>
         </div>
         </div>
-        <button onClick={getTxn}>gt</button>
     </section>
 }
 
